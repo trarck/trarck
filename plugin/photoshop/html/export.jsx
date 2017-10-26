70a37177402e9c778708317d969e8e2dcdddb739
trarck <trarck@gmail.com>
2017-10-26 18:12:35 +0800

    test export

---
+++ plugin/photoshop/html/export.jsx
@@ -0,0 +1,66 @@
+﻿ 
+ visitorLayers(app.activeDocument);
+ 
+function visitorLayers(obj){
+     showObjInfo(obj);
+    for( var i = 0; i < obj.artLayers.length; i++) {
+          visitorArtLayer(obj.artLayers[i]);
+    }     
+
+    for( var i = 0; i < obj.layerSets.length; i++) {
+        visitorLayerSet(obj.layerSets[i]);  // recursive call
+    }
+ }
+
+function visitorArtLayer(artLayer){
+    showArtLayerInfo (artLayer);
+    visitorLinedLayers(artLayer);
+}
+
+function visitorLayerSet(layerSet){
+    showLayerSetInfo (layerSet);
+    for( var i = 0; i < layerSet.artLayers.length; i++) {
+          visitorArtLayer(layerSet.artLayers[i]);
+    }     
+
+    for( var i = 0; i < layerSet.layerSets.length; i++) {
+        visitorLayerSet(layerSet.layerSets[i]);
+    }
+    visitorLinedLayers(layerSet);
+}
+
+function visitorLinedLayers(obj){
+    if(obj.linkedLayers && obj.linkedLayers.length){
+        for( var i = 0; i < obj.linkedLayers.length; i++) {
+            var layerObj=obj.linkedLayers[i];
+            switch(layerObj.typename){
+                    case "ArtLayer":
+                        visitorArtLayer(layerObj);
+                        break;
+                    case "LayerSet":
+                        visitorLayerSet(layerObj);
+                        break;
+            }
+        }
+    }
+ }
+
+function showObjInfo(obj){
+    var s="name:"+obj.name+"\n";
+    s+="typename:"+obj.typename;    
+    $.writeln(s);
+}
+
+function showArtLayerInfo(layerObj){
+    var s="name:"+layerObj.name+"\n";
+    s+="kind:"+layerObj.kind+"\n";
+    s+="linkedLayers:"+layerObj.linkedLayers.length+"\n";
+    s+="typename:"+layerObj.typename;    
+    $.writeln(s);
+}
+
+function showLayerSetInfo(layerObj){
+    var s="name:"+layerObj.name+"\n";
+    s+="typename:"+layerObj.typename;    
+    $.writeln(s);
+}

621bd4924248d1b6778b7b9d671e59c1ad66448e
trarck <trarck@gmail.com>
2018-07-24 18:26:54 +0800

    加入测试LayerStyle

---
+++ plugin/photoshop/tests/test_layerStyle.jsx
@@ -0,0 +1,14 @@
+﻿var g_LibsScriptFolderPath="/d/projects/trarck/plugin/photoshop/libs/";
+
+$.evalFile(g_LibsScriptFolderPath + "ps/Common.jsx");
+$.evalFile(g_LibsScriptFolderPath + "ps/Log.jsx");
+$.evalFile(g_LibsScriptFolderPath + "ps/defines/Terminology.jsx");
+$.evalFile(g_LibsScriptFolderPath + "ps/defines/Struct.jsx");
+$.evalFile(g_LibsScriptFolderPath + "ps/actions/ActionUtils.jsx");
+$.evalFile(g_LibsScriptFolderPath + "ps/layers/LayerStyle.jsx");
+
+var doc=app.activeDocument;
+var layer=doc.activeLayer;
+var ls=new LayerStyle(layer);
+ls.initialize();
+var i=0;
